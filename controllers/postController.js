0b8eb8b05d94d17a0a7904e207ad80cc045ad95d
LST <wer1264@gmail.com>
2021-10-06 17:57:49 +0900

    Read, Create 연습

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,12 @@
-const postUpload = (req, res) => {
-  return res.send("This is post page");
+const Post = require("../models/post.js");
+
+const postUpload = async (req, res) => {
+  const result = await Post.create({
+    title: "첫글!!",
+    content: "첫 글 입니다!!!",
+  });
+  console.log(result);
+  return res.end();
 };
 
 module.exports = {
--- controllers/rootController.js
+++ controllers/rootController.js
@@ -1,4 +1,8 @@
-const getPosts = (req, res) => {
+const Post = require("../models/post.js");
+
+const getPosts = async (req, res) => {
+  const result = await Post.findAll({});
+  console.log(result);
   return res.send("Get posts");
 };
 
